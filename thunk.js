1e00d9d4773f78079b37f565ec612a2aa938fcd9
gugol2 <jsanchezcriado@gmail.com>
2020-04-06 11:17:09 +0200

    Update thunk.js

--- thunk.js
+++ thunk.js
@@ -1,7 +1,7 @@
 // This is what the Thunk Middleware does behind the scenes
 // It allowes us to dispatch a function instead of an action object because
 // the middleware will call that function with dispatch method itself as the 
-// first argument and the reducer will only get a plain JS object.
+// first argument and the reducer will only get a plain JS action object.
 
 const thunk = (store) => (next) => (action) => {
     if(typeof action === 'function') {
